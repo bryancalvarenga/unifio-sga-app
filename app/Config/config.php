c19745add94a4a6fc8dca1aeecb701488b14dfba
bryancalvarenga <bryancalvarenga@outlook.com.br>
2025-09-18 20:36:57 -0300

    refactor: remove docker configs and restore local setup

--- app/Config/config.php
+++ app/Config/config.php
@@ -1,11 +1,21 @@
 <?php
-// Variáveis globais
+// Carrega o autoload do Composer
+require __DIR__ . '/../../vendor/autoload.php';
 
-define('BASE_PATH', dirname(__DIR__, 1));
+// Carrega variáveis do .env (na raiz do projeto)
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+$dotenv->load();
+
+
+// Variáveis globais
+define('BASE_PATH', dirname(__DIR__, 2));
+
 if (!defined('APP_PATH')) {
-    define('APP_PATH', ROOT_PATH . '/app');
+    define('APP_PATH', BASE_PATH . '/app');
 }
+
 if (!defined('VIEW_PATH')) {
     define('VIEW_PATH', APP_PATH . '/Views');
 }
+
 define('BASE_URL', $_ENV['BASE_URL'] ?? 'http://localhost:8000');
